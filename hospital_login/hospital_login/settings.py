1bd43c6d017fc868a0c0bca43cb7e886f8f637ab
Rushikesh Lakhotiya <rushikesh.lakhotiya01@gmail.com>
2022-04-24 23:50:06 +0530

    Setup of REST_FRAMEWORK pushed to rushikesh20030328

--- hospital_login/hospital_login/settings.py
+++ hospital_login/hospital_login/settings.py
@@ -39,7 +39,21 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'app',
     'hospital_login',
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
